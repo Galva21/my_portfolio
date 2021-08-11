2d17cbd6ca5288e452da39e82fd1e944983002ba
Alvaro <alvarojuan2014@gmail.com>
2021-08-11 11:42:45 -0400

    Comienzo del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Center(
+        child: Container(
+          child: Text(
+            'My Portfolio - Galva',
+            style: TextStyle(
+              color: Colors.blue
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
